034e77a918cfde0b9b21a97c384ed0a22b0fffca
toseth97 <oluwasheun9721@gmail.com>
2023-10-18 11:16:02 +0100

    finilizing space project

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,7 +8,7 @@ import Nav from './Nav'
 
 const Header = () => {
     return (
-        <header className='z-[20] py-2 flex w-full justify-between lg:px-24 md:px-16 px-8 items-center flex-wrap sticky top-0 bg-white/[.97] backdrop-blur'>
+        <header className='z-[20] flex w-full justify-between lg:px-24 md:px-16 px-8 items-center flex-wrap sticky top-0 bg-white/[.90] backdrop-blur'>
             <Logo />
             <Nav />
         </header>
